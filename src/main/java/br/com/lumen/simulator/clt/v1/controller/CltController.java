73ff51babfd97d0d7e6a6b01876a1d3deec631a3
matheus <matheusoliveira1991@hotmail.com>
2025-04-01 16:49:44 -0300

    restcontroller added

--- src/main/java/br/com/lumen/simulator/clt/v1/controller/CltController.java
+++ src/main/java/br/com/lumen/simulator/clt/v1/controller/CltController.java
@@ -4,13 +4,9 @@ import br.com.lumen.simulator.clt.v1.dto.CltDTO;
 import br.com.lumen.simulator.clt.v1.responses.CltResponse;
 import br.com.lumen.simulator.clt.v1.service.CltService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/")
 public class CltController {
 
