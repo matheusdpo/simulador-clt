4b8eb982f4d256189c36bf8ed2e5a730370fd1a2
matheus <matheusoliveira1991@hotmail.com>
2025-04-01 16:42:07 -0300

    corsconfig

---
+++ src/main/java/br/com/lumen/simulator/clt/v1/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package br.com.lumen.simulator.clt.v1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
