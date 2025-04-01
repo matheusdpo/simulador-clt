052379246b1472e0f9242d5a159a733aa453f831
matheus <matheusoliveira1991@hotmail.com>
2025-04-01 16:43:06 -0300

    added post mapping

--- src/main/java/br/com/lumen/simulator/clt/v1/controller/CltController.java
+++ src/main/java/br/com/lumen/simulator/clt/v1/controller/CltController.java
@@ -4,19 +4,20 @@ import br.com.lumen.simulator.clt.v1.dto.CltDTO;
 import br.com.lumen.simulator.clt.v1.responses.CltResponse;
 import br.com.lumen.simulator.clt.v1.service.CltService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api/v1/")
 public class CltController {
 
     @Autowired
     private CltService cltService;
 
-    @GetMapping("/clt")
+    @PostMapping("/clt")
     public CltResponse getSalarioLiquidoClt(@RequestBody CltDTO cltDTO) {
         return cltService.getSalarioLiquidoClt(cltDTO);
     }
